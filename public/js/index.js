7800eb0cd3fd76cce8c8f7d66ceb59edc34640ab
nellouille974 <lariviere.nelly@yahoo.fr>
2019-04-27 20:23:29 +0200

    Add command test

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	console.log("test");
+	// Exemple ajax request
+    // $('.js-like-article').on('click', function(e) {
+    //     $.ajax({
+    //         method: 'POST',
+    //         url: $link.attr('href')
+    //     }).done(function(data) {
+    //     	$('.js-like-article-count').html(data.hearts);
+    //     })
+    // });
 });
