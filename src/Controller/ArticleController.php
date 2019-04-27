543c00a930cf640760b4b2af7a48a82726749d1e
nellouille974 <lariviere.nelly@yahoo.fr>
2019-04-27 17:55:16 +0200

    Add first controller

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+    	return $this->render('public/index.html.twig', [
+        ]);
+    }
+}
